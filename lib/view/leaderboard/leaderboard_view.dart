7fbf11b7a34725c1004d910ac8840c6c707c227b
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-04-24 22:31:55 +0530

    changes in leadrboard

--- lib/view/leaderboard/leaderboard_view.dart
+++ lib/view/leaderboard/leaderboard_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 class LeaderboardView extends StatelessWidget {
@@ -8,17 +7,149 @@ class LeaderboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ranking'),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(
           horizontal: 16,
           vertical: 10,
         ),
         child: Column(
           children: [
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(
+                              'https://i.pinimg.com/564x/f9/23/68/f92368ac6737e58b34917a23ce35e82e.jpg',
+                            ),
+                          ),
+                          Text(
+                            'David',
+                            style: theme.textTheme.bodyLarge!.copyWith(
+                              color: theme.colorScheme.primary,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            '120',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Image.asset(
+                        'assets/images/pngwing.com (1).png',
+                        width: size.width * .2,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      height: size.height * .2,
+                      decoration: const BoxDecoration(),
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const CircleAvatar(
+                                radius: 35,
+                                backgroundImage: NetworkImage(
+                                  'https://i.pinimg.com/564x/f9/23/68/f92368ac6737e58b34917a23ce35e82e.jpg',
+                                ),
+                              ),
+                              Text(
+                                'David',
+                                style: theme.textTheme.bodyLarge!.copyWith(
+                                  color: theme.colorScheme.primary,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                '120',
+                                style: theme.textTheme.labelLarge!.copyWith(
+                                  color: theme.colorScheme.primary,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/pngwing.com (2).png',
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      height: size.height * .2,
+                      decoration: const BoxDecoration(),
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const CircleAvatar(
+                                radius: 35,
+                                backgroundImage: NetworkImage(
+                                  'https://i.pinimg.com/564x/f9/23/68/f92368ac6737e58b34917a23ce35e82e.jpg',
+                                ),
+                              ),
+                              Text(
+                                'David',
+                                style: theme.textTheme.bodyLarge!.copyWith(
+                                  color: theme.colorScheme.primary,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                '120',
+                                style: theme.textTheme.labelLarge!.copyWith(
+                                  color: theme.colorScheme.primary,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/pngwing.com (3).png',
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -38,40 +169,42 @@ class LeaderboardView extends StatelessWidget {
                 ),
               ],
             ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: 10,
-                separatorBuilder: (context, index) => const Gap(10),
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    contentPadding: const EdgeInsets.all(0),
-                    
-                    leading: Row(
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 10,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    leading: Text(
+                      '${index + 4}',
+                      style: theme.textTheme.bodyLarge!.copyWith(
+                        color: theme.colorScheme.onBackground,
+                      ),
+                    ),
+                    title: Row(
                       children: [
-                        Text(
-                          "${index + 1}",
-                          style: theme.textTheme.labelLarge!.copyWith(
-                            color: theme.colorScheme.onBackground,
-                          ),
-                        ),
                         const CircleAvatar(
-                          radius: 30,
                           backgroundImage: NetworkImage(
                             'https://i.pinimg.com/564x/a7/fc/12/a7fc121134320e11efa0d989b93c7a42.jpg',
                           ),
                         ),
+                        const Gap(5),
+                        Text(
+                          'Daniel',
+                          style: theme.textTheme.labelLarge!.copyWith(
+                            color: theme.colorScheme.onBackground,
+                          ),
+                        ),
                       ],
                     ),
-                    title: Text(
-                      'Daniel',
-                      style: theme.textTheme.labelLarge!.copyWith(
-                        color: theme.colorScheme.onBackground,
-                      ),
-                    ),
                     trailing: const Text('30'),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
           ],
         ),
--- lib/view/profile/profile_view.dart
+++ lib/view/profile/profile_view.dart
@@ -5,6 +5,13 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Column(children: [
+        
+      ],),
+    );
   }
 }
